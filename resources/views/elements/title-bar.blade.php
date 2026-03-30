e11fcb78df250ad1aed2e92e7ba480560bca44e5
Olivier Grandmougin <olivier.grandmougin@proman-interim.com>
2026-03-30 22:06:18 +0200

    chore: Add link to official repo

--- resources/views/elements/title-bar.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-$color = $color ?? 'sky';
-$t = $t ?? '';
-?>
-<div class="flex px-1">
-    <span class="bg-gradient-to-l from-{{$color}}-600 to-{{$color}}-800 w-20"></span>
-    <span class="bg-{{$color}}-600 text-{{$color}}-200 flex-1 text-center">{{$t}}</span>
-    <span class="bg-gradient-to-r from-{{$color}}-600 to-{{$color}}-800 w-20"></span>
-</div>
--- src/Async/TaskException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace OmniTerm\Async;
-
-use Exception;
-
-final class TaskException extends Exception {}
